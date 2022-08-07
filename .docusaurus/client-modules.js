3610b92dfe7323e763adfe144c14c392453d3354
Michal <michalwols@gmail.com>
2022-08-07 11:02:50 -0400

    remove .docusaurus dir

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/michalwolski/projects/kb/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/michalwolski/projects/kb/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/michalwolski/projects/kb/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/michalwolski/projects/kb/src/css/custom.css'),
-];
