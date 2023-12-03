01cf6158351f511896dae23c5c2c10f0079c473d
michalwols <michalwols@users.noreply.github.com>
2023-12-03 19:08:23 +0000

    deploy: 6abc01f2a2b94c13e7f50fdcfa787159e3098ba4

---
+++ assets/js/c16fd2d1.2b67a55a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkb=self.webpackChunkkb||[]).push([[2610],{5496:s=>{s.exports=JSON.parse('{"title":"Business","slug":"/category/business","permalink":"/docs/category/business","navigation":{"previous":{"title":"optimization","permalink":"/docs/math/optimization"},"next":{"title":"accounting","permalink":"/docs/business/accounting"}}}')}}]);
--- assets/js/c16fd2d1.b964b029.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkkb=self.webpackChunkkb||[]).push([[2610],{5496:s=>{s.exports=JSON.parse('{"title":"Business","slug":"/category/business","permalink":"/docs/category/business","navigation":{"previous":{"title":"Optimization","permalink":"/docs/math/optimization"},"next":{"title":"accounting","permalink":"/docs/business/accounting"}}}')}}]);
